39d3ccd3323acfac236b3c3faf42ff862382cf77
ryan-kim21 <63631441+ryan-kim21@users.noreply.github.com>
2023-06-26 22:36:10 +0900

    ㅅㅅㅅ

--- internet_gateway.tf
+++ internet_gateway.tf
@@ -2,7 +2,7 @@ resource "aws_internet_gateway" "test-internet-gateway" {
 
   depends_on = [
     aws_vpc.test-vpc
-  ]
+  ] //VPC_ID가 생성되야 internet_gateway가 생성됨
 
   vpc_id = aws_vpc.test-vpc.id
 }
